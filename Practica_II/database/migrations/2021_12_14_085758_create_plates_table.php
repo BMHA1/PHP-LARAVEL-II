526a670af4f872c9ee036184c747a1c5a749460b
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-12-14 10:08:23 +0100

    creamos dos tablas : platos y tipo de platos

---
+++ Practica_II/database/migrations/2021_12_14_085758_create_plates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_tipo_plates');
+            $table->string('nombre');
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plates');
+    }
+}
